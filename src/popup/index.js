6c5f0632ab7f0765f342d13cc568b83c64a4c85a
rugk <rugk+git@posteo.de>
2019-03-20 12:05:00 +0100

    Switch to prebuild version of emoji-mart with preact & remount

--- src/popup/index.js
+++ src/popup/index.js
@@ -3,26 +3,5 @@
  *
  */
 "use strict";
-/* globals React ReactDOM */
 
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
 
-    render() {
-        if (this.state.liked) {
-            return "You liked this.";
-        }
-
-        return React.createElement(
-            "button",
-            { onClick: () => this.setState({ liked: true }) },
-            "Like"
-        );
-    }
-}
-debugger;
-const domContainer = document.querySelector("#reactContainer");
-ReactDOM.render(React.createElement(Picker), domContainer);
