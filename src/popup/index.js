77dee5823c297bff8cb039dddc82a57466363c8d
rugk <rugk+git@posteo.de>
2019-03-23 11:40:15 +0100

    Remove tried workaround

--- src/popup/index.js
+++ src/popup/index.js
@@ -16,15 +16,6 @@ async function createPicker() {
     await EmojiPicker.init(
         await EmojiSettings.getAllSettings()
     ).catch(console.error);
-
-    window.setTimeout(() => {
-        // EmojiPicker.setAttribute({autoFocus: false});
-        // EmojiPicker.setAttribute({autoFocus: true});
-    }, 1000);
-        window.setTimeout(() => {
-            // EmojiPicker.setAttribute({autoFocus: false});
-            EmojiPicker.setAttribute({autoFocus: true});
-        }, 2000);
 }
 
 createPicker();
