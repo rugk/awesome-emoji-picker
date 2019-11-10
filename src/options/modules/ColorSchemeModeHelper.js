0f6516214f6fd520cf13a3ef04856dc47a465fb6
silvererudite <shamima.hossain19@gmail.com>
2019-11-10 16:44:24 +0600

    attempt to create an init method for changeScreenshotTheme() and removing some whitespaces

--- src/options/modules/ColorSchemeModeHelper.js
+++ src/options/modules/ColorSchemeModeHelper.js
@@ -8,14 +8,20 @@
  * @param  {Object} darkQuery
  */
 const dark = window.matchMedia("(prefers-color-scheme: dark)");
-export function changeScreenshotTheme(darkQuery){
+function changeScreenshotTheme(darkQuery){
     if (darkQuery.matches) {
         document.getElementById("searchBarDemo").src = "./img/emojiSearchDog_dark.png";
     } else {
         document.getElementById("searchBarDemo").src = "./img/emojiSearchDog_light.png";
     }
-    return ColorSchemeModeHelper.changeScreenshotTheme(darkQuery);
 }
 
 dark.addListener(changeScreenshotTheme);
-changeScreenshotTheme(dark);
+/**
+ * Returns the function which changes screenshot according to theme
+ *
+ * @public
+ */
+export function init(){
+    return changeScreenshotTheme(dark);
+}
