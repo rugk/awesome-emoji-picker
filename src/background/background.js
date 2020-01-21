rugk/awesome-emoji-picker

commit 085c7b9d505aecf5a87da478d4ccd80f3f63990b

Add first initial implementation of Emoji search in address ba
(called omnibar)

src/background/background.js
@@ -1,4 +1,9 @@
-import * as IconHandler from "/common/modules/IconHandler.js";
+import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
+
+import * as OmniboxSearch from "./modules/OmniboxSearch.js";
 
 // init modules
-IconHandler.init();
+const emojiSearch = AddonSettings.get("emojiSearch");
+if (emojiSearch) {
+    OmniboxSearch.init();
+}

src/background/modules/OmniboxSearch.js
@@ -1,0 +1,94 @@
+import * as EmojiInteraction from "/common/modules/EmojiInteraction.js";
+
+/**
+ * Lazy-load the emoji-mart library, .
+ *
+ * This consumes some memory (RAM), up-to 10MB, as remount and other things are loaded.
+ *
+ * @public
+ * @returns {void}
+ */
+function loadEmojiMart() {
+    const emojiMartLoader = document.createElement("script");
+    emojiMartLoader.setAttribute("async", true);
+    emojiMartLoader.setAttribute("src", "/common/lib/emoji-mart-embed/dist/emoji-mart.js");
+    document.querySelector("head").appendChild(emojiMartLoader);
+}
+
+/**
+ * Trigger the evaluation for the search for emojis.
+ *
+ * @public
+ * @param {string} text the string the user entered
+ * @param {function} suggest function to call to add suggestions
+ * @returns {void}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/omnibox/onInputChanged}
+ */
+export function triggerOmnixboxSuggestion(text, suggest) {
+    const suggestions = window.emojiMart.emojiIndex.search(text).map((emoji) => {
+        return {
+            description: browser.i18n.getMessage("searchResultDescription", [
+                emoji.native,
+                emoji.name,
+                emoji.colons
+            ]),
+            content: emoji.native
+        };
+    });
+
+    suggest(suggestions);
+}
+
+/**
+ * Triggered when the search is actually executed,.
+ *
+ * @public
+ * @param {string} text the string the user entered or selected
+ * @param {string} disposition how the result should be possible
+ * @returns {void}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/omnibox/onInputEntered}
+ */
+export function triggerOmnixboxSearch(text) {
+    const searchResult = window.emojiMart.emojiIndex.search(text);
+
+    // if a single emoji is selected or searched for, detect this and return
+    // emoji data
+    try {
+        searchResult.push(
+            window.emojiMart.getEmojiDataFromNative(text)
+        );
+    } catch (e) {
+        // ignore errors, as we usually expect text strings there and these are
+        // totally fine, too; search may find something here
+    }
+
+    // emoji itself copied or found
+    if (searchResult.length === 1) {
+        // if result is only one emoji, also instantly copy it
+        EmojiInteraction.copyEmoji(searchResult[0]);
+    } else {
+        // otherwise open popup to show all emoji choices
+        browser.browserAction.openPopup();
+    }
+
+}
+
+/**
+ * Init omnibox search.
+ *
+ * @public
+ * @returns {void}
+ */
+export function init() {
+    // lazy-load emoji-mart
+    loadEmojiMart();
+
+    browser.omnibox.onInputChanged.addListener(triggerOmnixboxSuggestion);
+    browser.omnibox.onInputEntered.addListener(triggerOmnixboxSearch);
+
+    browser.omnibox.setDefaultSuggestion({
+        description: browser.i18n.getMessage("searchTipDescription", [
+            browser.i18n.getMessage("extensionName")
+        ])
+    });
+}

src/common/modules/EmojiInteraction.js
@@ -1,0 +1,72 @@
+import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
+
+const POPUP_ICON_OPTION = "popupIconColored";
+
+/**
+ * Sets a popup icon variant.
+ *
+ * @private
+ * @param {string} icon version or "null"/"undefined" to reset to default
+ * @returns {Promise}
+ */
+function setPopupIcon(icon) {
+    // verify parameter
+    switch (icon) {
+    case "dark": // fall through
+    case "light":
+    case "colored":
+    case null:
+        // ok
+        break;
+    default:
+        throw new TypeError(`invalid parameter: ${icon}`);
+    }
+
+    // ignore request if API is not available
+    if (browser.browserAction.setIcon === undefined) {
+        return Promise.resolve();
+    }
+
+    if (icon === null || icon === undefined) {
+        return browser.browserAction.setIcon({path: null});
+    }
+
+    // set colored icon
+    if (icon === "colored") {
+        // WTF: For whatever reason, these paths need to be absolute...
+        return browser.browserAction.setIcon({path: {
+            "16": "/icons/icon_32.png",
+            "32": "/icons/icon_32.png",
+            "64": "/icons/icon_64.png",
+            "128": "/icons/icon_128.png"
+        }});
+    }
+
+    return browser.browserAction.setIcon({path: `/icons/fa-grin-${icon}.svg`});
+}
+
+/**
+ * Set icon depending on whether it should be colored, or not.
+ *
+ * @public
+ * @param {boolean} popupIconColored if popupIconColored is colored or not
+ * @returns {Promise}
+ */
+export function changeIconIfColored(popupIconColored) {
+    if (popupIconColored === true) {
+        return setPopupIcon("colored");
+    } else {
+        // reset icon
+        return setPopupIcon(null);
+    }
+}
+
+/**
+ * Init icon module.
+ *
+ * @public
+ * @returns {void}
+ */
+export function init() {
+    return AddonSettings.get(POPUP_ICON_OPTION).then((popupIconColored) => changeIconIfColored(popupIconColored));
+}

src/common/modules/IconHandler.js
@@ -1,72 +1,22 @@
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 
-const POPUP_ICON_OPTION = "popupIconColored";
-
 /**
- * Sets a popup icon variant.
+ * Copy the Emoji to clipboard, once it has been selected.
  *
  * @private
- * @param {string} icon version or "null"/"undefined" to reset to default
- * @returns {Promise}
- */
-function setPopupIcon(icon) {
-    // verify parameter
-    switch (icon) {
-    case "dark": // fall through
-    case "light":
-    case "colored":
-    case null:
-        // ok
-        break;
-    default:
-        throw new TypeError(`invalid parameter: ${icon}`);
-    }
-
-    // ignore request if API is not available
-    if (browser.browserAction.setIcon === undefined) {
-        return Promise.resolve();
-    }
-
-    if (icon === null || icon === undefined) {
-        return browser.browserAction.setIcon({path: null});
-    }
-
-    // set colored icon
-    if (icon === "colored") {
-        // WTF: For whatever reason, these paths need to be absolute...
-        return browser.browserAction.setIcon({path: {
-            "16": "/icons/icon_32.png",
-            "32": "/icons/icon_32.png",
-            "64": "/icons/icon_64.png",
-            "128": "/icons/icon_128.png"
-        }});
-    }
-
-    return browser.browserAction.setIcon({path: `/icons/fa-grin-${icon}.svg`});
-}
-
-/**
- * Set icon depending on whether it should be colored, or not.
- *
- * @public
- * @param {boolean} popupIconColored if popupIconColored is colored or not
- * @returns {Promise}
- */
-export function changeIconIfColored(popupIconColored) {
-    if (popupIconColored === true) {
-        return setPopupIcon("colored");
-    } else {
-        // reset icon
-        return setPopupIcon(null);
-    }
-}
-
-/**
- * Init icon module.
- *
- * @public
+ * @param {Object} emoji
  * @returns {void}
  */
-export function init() {
-    return AddonSettings.get(POPUP_ICON_OPTION).then((popupIconColored) => changeIconIfColored(popupIconColored));
+export async function copyEmoji(emoji) {
+    const emojiCopyOption = await AddonSettings.get("copyEmoji");
+    switch (emojiCopyOption) {
+    case "native":
+        navigator.clipboard.writeText(emoji.native);
+        break;
+    case "colons":
+        navigator.clipboard.writeText(emoji.colons);
+        break;
+    default:
+        throw new Error("invalid option:", "copyEmoji", emojiCopyOption);
+    }
 }

src/common/modules/data/DefaultSettings.js
@@ -14,5 +14,6 @@ export const DEFAULT_SETTINGS = Object.freeze({
         emojiSize: 24
     },
     copyEmoji: "native",
-    emojiMart: {}
+    emojiMart: {},
+    emojiSearch: true
 });

src/popup/index.html
@@ -5,12 +5,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="../common/common.css">
 		<link rel="stylesheet" href="index.css">
-		<link rel="stylesheet" href="./lib/emoji-mart-embed/dist/emoji-mart.css">
+		<link rel="stylesheet" href="/common/lib/emoji-mart-embed/dist/emoji-mart.css">
 
 		<script defer src="../common/common.js" type="module" charset="utf-8"></script>
 		<script defer src="index.js" type="module" charset="utf-8"></script>
 		<!-- we need to load emoji-mart last, so autoFocus works -->
-		<script defer src="./lib/emoji-mart-embed/dist/emoji-mart.js"></script>
+		<script defer src="/common/lib/emoji-mart-embed/dist/emoji-mart.js"></script>
 	</head>
 	<body>
 		<head>

src/popup/module/EmojiPicker.js
@@ -4,7 +4,7 @@
  * @public
  */
 
-import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
+import * as EmojiInteraction from "/common/modules/EmojiInteraction.js";
 
 let emojiPicker = null;
 
@@ -19,7 +19,7 @@ const EMOJI_SHEET_DIR = "/popup/img/emoji-images";
 export const hardcodedSettings = Object.freeze({
     i18n: getEmojiMartLocalised(),
     autoFocus: true,
-    onSelect: copyEmoji,
+    onSelect: EmojiInteraction.copyEmoji,
     style: { "border": "none" },
     backgroundImageFn: getEmojiSheet,
     // emojiSize: 64
@@ -61,27 +61,6 @@ function getEmojiMartLocalised() {
             6: browser.i18n.getMessage("emojiMartSkintone6"),
         }
     };
-}
-
-/**
- * Copy the Emoji to clipboard, once it has been selected.
- *
- * @private
- * @param {Object} emoji
- * @returns {void}
- */
-async function copyEmoji(emoji) {
-    const emojiCopyOption = await AddonSettings.get("copyEmoji");
-    switch (emojiCopyOption) {
-    case "native":
-        navigator.clipboard.writeText(emoji.native);
-        break;
-    case "colons":
-        navigator.clipboard.writeText(emoji.colons);
-        break;
-    default:
-        throw new Error("invalid option:", "copyEmoji", emojiCopyOption);
-    }
 }
 
 /**

commit 046416df6ee5a06f6af5b485b6fab0ed3a4d19ad

Fix icon loading
Fixes https://github.com/rugk/awesome-emoji-picker/issues/75
Broken by 085c7b9d505aecf5a87da478d4ccd80f3f63990b

src/background/background.js
@@ -1,4 +1,6 @@
+import * as IconHandler from "/common/modules/IconHandler.js";
 import * as OmniboxSearch from "./modules/OmniboxSearch.js";
 
 // init modules
+IconHandler.init();
 OmniboxSearch.init();