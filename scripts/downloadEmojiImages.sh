33def8c2a26e7c03ca4fabe816e275d4f7a83af6
rugk <rugk+git@posteo.de>
2025-04-10 19:33:26 +0200

    wipfeat: re-implement local image download/URL for assets

--- scripts/downloadEmojiImages.sh
+++ scripts/downloadEmojiImages.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
-# Updates the downloaded emoji.
+# Updates the downloaded emoji. Execute this from the root directory.
 #
 # Syntax: scripts/downloadEmojiImages.sh emojiDataSourceVersion
 # emojiDataSourceVersion – selects the version from npm, e.g. "latest" or a version number
 #
+# URL from:
+# https://github.com/missive/emoji-mart/blob/16978d04a766eec6455e2e8bb21cd8dc0b3c7436/packages/emoji-mart/src/components/Emoji/Emoji.tsx#L35
 
 # hardcoded
 USER_AGENT="awesome-emoji-picker browser add-on build script (https://github.com/rugk/awesome-emoji-picker)"
@@ -13,11 +15,14 @@ EMOJI_SETS="apple google twitter facebook"
 STORAGE_DIRECTORY="./src/popup/img/emoji-images"
 
 # default
-emojiDataSourceVersion="5.0.1"
+emojiDataSourceVersion="15.0.1"
 test -n "$1" && emojiDataSourceVersion="$1" && echo "Using version $emojiDataSourceVersion for emoji-datasource."
 
 for set in $EMOJI_SETS; do
+    url="https://cdn.jsdelivr.net/npm/emoji-datasource-${set}@${emojiDataSourceVersion}/img/${set}/sheets-256/${SHEET_SIZE}.png"
+    output="$STORAGE_DIRECTORY/${set}-${SHEET_SIZE}.png"
+    echo "Downloading \"$url\" to \"${output}\"…"
     wget --user-agent "$USER_AGENT" \
-        "https://unpkg.com/emoji-datasource-${set}@${emojiDataSourceVersion}/img/${set}/sheets-256/${SHEET_SIZE}.png" \
-        -O "$STORAGE_DIRECTORY/${set}-${SHEET_SIZE}.png";
+        "$url" \
+        -O "$output";
 done
--- src/popup/module/EmojiPicker.js
+++ src/popup/module/EmojiPicker.js
@@ -25,7 +25,7 @@ export const hardcodedSettings = Object.freeze({
     // onClickOutside: EmojiSelect.saveClickPosition, // TOOD: ??
     // style: { border: "none" },
     theme: "auto",
-    backgroundImageFn: getEmojiSheet,
+    getSpritesheetURL: getEmojiSheet,
     // title: browser.i18n.getMessage("extensionNameShort"), // show the extension name by default
     dynamicWidth: false, // will not work with WebExtension popups, the content defines the width
     // emojiButtonColors:  // TODO: ??,
@@ -35,18 +35,23 @@ export const hardcodedSettings = Object.freeze({
 /**
  * Return the emoji sheet to use.
  *
+ * Called from:
+ * @link {https://github.com/missive/emoji-mart/blob/16978d04a766eec6455e2e8bb21cd8dc0b3c7436/packages/emoji-mart/src/components/Emoji/Emoji.tsx#L34}
+ *
+ * For more information see the download script in the `/scripts` directory.
+ *
  * @private
  * @param {string} set
- * @param {string} sheetSize
  * @returns {string} the URL to the emoji sheet
  */
-function getEmojiSheet(set, sheetSize) {
+function getEmojiSheet(set) {
+    const sheetSize = 64;
     // returns local saved version to speed up loading
     return browser.runtime.getURL(`${EMOJI_SHEET_DIR}/${set}-${sheetSize}.png`);
 
     // default online source would be this one
     // const EMOJI_DATASOURCE_VERSION = "latest"; // with a fixed version, however
-    // return `https://unpkg.com/emoji-datasource-${set}@${EMOJI_DATASOURCE_VERSION}/img/${set}/sheets-256/${sheetSize}.png`;
+    // return `https://cdn.jsdelivr.net/npm/emoji-datasource-${set}@${EMOJI_DATASOURCE_VERSION}/img/${set}/sheets-256/64.png`;
 }
 
 /**
