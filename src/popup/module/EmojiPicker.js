fea40ab1dbfa6c1c74db9cd08dc38b1ee40ddd20
rugk <rugk+git@posteo.de>
2025-04-10 19:11:32 +0200

    chore: remove dead code

--- src/popup/module/EmojiPicker.js
+++ src/popup/module/EmojiPicker.js
@@ -33,44 +33,6 @@ export const hardcodedSettings = Object.freeze({
 });
 
 /**
- * Return the translated strings for emoji-mart.
- *
- * @private
- * @returns {Object}
- * @see https://github.com/missive/emoji-mart#i18n
- */
-function getEmojiMartLocalised() {
-    return {
-        search: browser.i18n.getMessage("emojiMartSearch"),
-        clear: browser.i18n.getMessage("emojiMartClear"), // Accessible label on "clear" button
-        notfound: browser.i18n.getMessage("emojiMartNoEmojiFound"),
-        skintext: browser.i18n.getMessage("emojiMartSkinText"),
-        categories: {
-            search: browser.i18n.getMessage("emojiMartCategorySearch"),
-            recent: browser.i18n.getMessage("emojiMartCategoryRecent"),
-            people: browser.i18n.getMessage("emojiMartCategoryPeople"),
-            nature: browser.i18n.getMessage("emojiMartCategoryNature"),
-            foods: browser.i18n.getMessage("emojiMartCategoryFoods"),
-            activity: browser.i18n.getMessage("emojiMartCategoryActivity"),
-            places: browser.i18n.getMessage("emojiMartCategoryPlaces"),
-            objects: browser.i18n.getMessage("emojiMartCategoryObjects"),
-            symbols: browser.i18n.getMessage("emojiMartCategorySymbols"),
-            flags: browser.i18n.getMessage("emojiMartCategoryFlags"),
-            custom: browser.i18n.getMessage("emojiMartCategoryCustom")
-        },
-        categorieslabel: browser.i18n.getMessage("emojiMartCategoriesLabel"), // Accessible title for the list of categories
-        skintones: {
-            1: browser.i18n.getMessage("emojiMartSkintone1"),
-            2: browser.i18n.getMessage("emojiMartSkintone2"),
-            3: browser.i18n.getMessage("emojiMartSkintone3"),
-            4: browser.i18n.getMessage("emojiMartSkintone4"),
-            5: browser.i18n.getMessage("emojiMartSkintone5"),
-            6: browser.i18n.getMessage("emojiMartSkintone6")
-        }
-    };
-}
-
-/**
  * Return the emoji sheet to use.
  *
  * @private
