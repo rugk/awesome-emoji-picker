c1e1e8dbac8de00321bac9937b9ef28630be1d84
rugk <rugk+git@posteo.de>
2025-04-19 10:32:07 +0200

    chore: remove unused var

--- src/popup/module/EmojiPicker.js
+++ src/popup/module/EmojiPicker.js
@@ -7,8 +7,6 @@
 import * as EmojiSelect from "./EmojiSelect.js";
 import * as EmojiMart from "../../node_modules/emoji-mart/dist/module.js";
 
-let emojiPicker = null;
-
 const EMOJI_SHEET_DIR = "/popup/img/emoji-images";
 
 /**
