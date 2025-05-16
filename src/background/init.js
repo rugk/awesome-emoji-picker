7d9e8bc0cba2bd2cb35ec323d4727bb105cc707b
rugk <rugk+git@posteo.de>
2025-05-16 21:38:16 +0200

    wipfix: re-add ContextMenu integration (load file again)

--- src/background/init.js
+++ src/background/init.js
@@ -1,9 +1,11 @@
 import * as IconHandler from "/common/modules/IconHandler.js";
 import * as AutocorrectHandler from "/common/modules/AutocorrectHandler.js";
+import * as ContextMenu from "./modules/ContextMenu.js";
 
 browser.runtime.onInstalled.addListener(() => {
     IconHandler.init();
     AutocorrectHandler.init();
+    ContextMenu.init();
 
     console.warn("background: init loaded");
 });
