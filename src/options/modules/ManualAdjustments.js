c15394ee797d184ac0ba64aa1abffe16b9f84512
theluckystrike <51033404+theluckystrike@users.noreply.github.com>
2026-03-04 08:07:20 +0700

    docs: update Chrome Web Store URLs to new format
    Google has migrated the Chrome Web Store to chromewebstore.google.com.
    Updates the 2 CWS URLs in source files to use the new domain:
    - src/options/modules/ManualAdjustments.js (Unicodify link)
    - src/common/modules/data/Tips.js (review prompt link)
    
    The old URLs currently redirect but may stop working as Google
    phases out the legacy domain.

--- src/options/modules/ManualAdjustments.js
+++ src/options/modules/ManualAdjustments.js
@@ -33,7 +33,7 @@ export function init() {
     getBrowserValue({
         firefox: "https://addons.mozilla.org/firefox/addon/unicodify-text-transformer/?utm_source=awesomeEmojiPicker-addon&utm_medium=addon&utm_content=awesomeEmojiPicker-addon-settings-inline&utm_campaign=awesomeEmojiPicker-addon-settings-inline",
         thunderbird: "https://addons.thunderbird.net/thunderbird/addon/unicodify-text-transformer/?utm_source=awesomeEmojiPicker-addon&utm_medium=addon&utm_content=awesomeEmojiPicker-addon-settings-inline&utm_campaign=awesomeEmojiPicker-addon-settings-inline",
-        chrome: "https://chrome.google.com/webstore/detail/unicodify-text-transformer/?utm_source=awesomeEmojiPicker-addon&utm_medium=addon&utm_content=awesomeEmojiPicker-addon-settings-inline&utm_campaign=awesomeEmojiPicker-addon-settings-inline"
+        chrome: "https://chromewebstore.google.com/detail/unicodify-text-transformer/?utm_source=awesomeEmojiPicker-addon&utm_medium=addon&utm_content=awesomeEmojiPicker-addon-settings-inline&utm_campaign=awesomeEmojiPicker-addon-settings-inline"
     }).then((browserUrl) => {
         document.getElementById("link-unicodify").href = browserUrl;
     });
